e0a993e2edec937dfba61de3c6ec4d3ca94b8b08
Sebastien Kawada <coolkid12345pro@gmail.com>
2025-11-08 22:54:19 -0800

    fix: drop deprecated send_example_telemetry import

--- src/process_dataset.py
+++ src/process_dataset.py
@@ -45,7 +45,7 @@ from transformers import (
 )
 from transformers.testing_utils import CaptureLogger
 from transformers.trainer_utils import get_last_checkpoint
-from transformers.utils import check_min_version, send_example_telemetry
+from transformers.utils import check_min_version
 from transformers.utils.versions import require_version
 from datasets import Features, Sequence, Value
 
